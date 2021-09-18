7c821dd79b1ed34e549d40cab891cb765ffefd9b
Laineda <90093949+Laineda@users.noreply.github.com>
2021-09-18 15:29:56 +0300

    2 variantas

--- 1programa/1programa.cpp
+++ 1programa/1programa.cpp
@@ -1,14 +1,18 @@
 ﻿#include <iostream>
+#include <iomanip>
 #include <string>
 using std::cout;
 using std::cin;
 using std::endl;
 using std::string;
+using std::setw;
+using std::setprecision;
 
 struct studentas {
     string vardas, pavarde;
     float nd[10]{};
     float egzaminas;
+    float galutinis;
 }; 
 
 void pild(studentas &kint);
@@ -17,9 +21,9 @@ void print(studentas &kin);
 int main()
 {
     studentas studentai[5];
-    for (int i = 0; i < 5; i++)
+    for (int i = 0; i < 1; i++)
         pild(studentai[i]);
-    for (int i = 0; i < 5; i++)
+    for (int i = 0; i < 1; i++)
         print(studentai[i]);
 }
 
@@ -28,16 +32,19 @@ void pild(studentas& kint) {
     cout << "Iveskite studento varda ir pavarde: "; cin >> kint.vardas >>kint.pavarde;
     cout << "Iveskite namu darbu kieki (1-10): "; cin >> n;
     cout << "Iveskite namu darbu pazymius: "<<endl;
+    float sum = 0, vidurkis = 0;
     for (int i = 0; i < n; i++) {
         cin >> kint.nd[i]; 
+        sum += kint.nd[i];
     }
+    vidurkis = sum / n;
     cout << "Iveskite egzamino pazymi: "; cin >> kint.egzaminas;
+    kint.galutinis = vidurkis * 0.4 + kint.egzaminas*0.6;
 }
 
-void print(studentas& kin) {
-    cout << kin.vardas << " " << kin.pavarde << " ";
-    for (int i = 0; i < (sizeof(kin.nd) / sizeof(kin.nd[0])); i++)
-        cout << kin.nd[i] << " ";
-    cout << "Egzaminas: " << kin.egzaminas << endl;
+void print(studentas &kint) {
+    cout << setw(20) << "Pavarde" << setw(20) << "Vardas" << setw(20) << "Galutinis(Vid.)" << endl;
+    cout << setw(20) << kint.pavarde << setw(20) << kint.vardas;
+    cout << setw(20) << setprecision(3) << kint.galutinis << endl;
 }
 
