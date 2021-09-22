54832b83fe3fdd02d18aebfb937bf21c1091102c
Laineda <90093949+Laineda@users.noreply.github.com>
2021-09-23 01:12:37 +0300

    Update 1programa.cpp

--- 1programa/1programa.cpp
+++ 1programa/1programa.cpp
@@ -69,7 +69,7 @@ void pild(studentas& kint)
 }
 
 void print(studentas& kint) {
-    cout << left << setw(20) << "Pavarde" << setw(20) << "Vardas" << setw(20) << "Galutinis(Vid.)" << endl;
+    cout << left << setw(20) << "Pavarde" << setw(20) << "Vardas" << setw(20) << "Galutinis(Vid.) / Galutinis(Med.)" << endl;
     cout << "------------------------------------------------------------------" << endl;
     cout << setw(20) << kint.pavarde << setw(20) << kint.vardas;
     cout << setw(20) << setprecision(3) << kint.galutinis << endl;
