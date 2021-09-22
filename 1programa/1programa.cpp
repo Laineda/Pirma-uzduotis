408f551233164d936315ef47d20b57b8770ad4ae
Laineda <90093949+Laineda@users.noreply.github.com>
2021-09-23 01:08:19 +0300

    3 variantas

--- 1programa/1programa.cpp
+++ 1programa/1programa.cpp
@@ -1,6 +1,8 @@
 ﻿#include <iostream>
 #include <iomanip>
 #include <string>
+#include <algorithm>
+#include <numeric>
 using std::cout;
 using std::cin;
 using std::endl;
@@ -8,16 +10,18 @@ using std::string;
 using std::setw;
 using std::setprecision;
 using std::left;
+using std::sort;
+
 
 struct studentas {
     string vardas, pavarde;
-    float nd[10]{};
-    float egzaminas;
-    float galutinis;
-}; 
+    double nd[10]{};
+    double egzaminas;
+    double galutinis;
+};
 
-void pild(studentas &kint);
-void print(studentas &kin);
+void pild(studentas& kint);
+void print(studentas& kin);
 
 int main()
 {
@@ -28,25 +32,48 @@ int main()
         print(studentai[i]);
 }
 
-void pild(studentas& kint) {
-    int n;
-    cout << "Iveskite studento varda ir pavarde: "; cin >> kint.vardas >>kint.pavarde;
-    cout << "Iveskite namu darbu kieki (1-10): "; cin >> n;
-    cout << "Iveskite namu darbu pazymius: "<<endl;
-    float sum = 0, vidurkis = 0;
-    for (int i = 0; i < n; i++) {
-        cin >> kint.nd[i]; 
+void pild(studentas& kint) 
+{
+    int n; 
+    cout << "Iveskite studento varda ir pavarde: "; cin >> kint.vardas >> kint.pavarde;
+    cout << "Kiek namu darbu bus? (1-10): "; cin >> n; 
+    cout << "Iveskite namu darbu pazymius: " << "\n";
+    double sum = 0; 
+    double vidurkis = 0; 
+    double mediana = 0;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> kint.nd[i];
         sum += kint.nd[i];
+    }
+    sort(kint.nd, kint.nd + n);
+    if (n % 2 != 0)
+    {
+        mediana = (double)kint.nd[n / 2];
+    }
+    else
+    {
+        mediana = (double)(kint.nd[(n - 1) / 2] + kint.nd[n / 2]) / 2.0;
     }
     vidurkis = sum / n;
     cout << "Iveskite egzamino pazymi: "; cin >> kint.egzaminas;
-    kint.galutinis = vidurkis * 0.4 + kint.egzaminas*0.6;
+    int m; 
+    cout << "Jei norite, jog galutinis pazymys butu skaiciuojamas su vidurkiu iveskite 0, o jei su mediana parasykite 1: "; cin >> m; cout << endl;
+    if (m == 0) {
+        kint.galutinis = 0.4 * vidurkis + 0.6 * kint.egzaminas; 
+    }
+    else {
+        kint.galutinis = 0.4 * mediana + 0.6 * kint.egzaminas;
+    }
+
 }
 
-void print(studentas &kint) {
+void print(studentas& kint) {
     cout << left << setw(20) << "Pavarde" << setw(20) << "Vardas" << setw(20) << "Galutinis(Vid.)" << endl;
     cout << "------------------------------------------------------------------" << endl;
     cout << setw(20) << kint.pavarde << setw(20) << kint.vardas;
     cout << setw(20) << setprecision(3) << kint.galutinis << endl;
+    cout << "----------------------------------------------------------------------------------------" << endl;
 }
+
 
