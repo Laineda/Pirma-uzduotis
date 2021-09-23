7a685e4b0e27a9d40086e49119f646616ad21d19
Laineda <90093949+Laineda@users.noreply.github.com>
2021-09-24 00:07:12 +0300

    4 variantas (std::vector)

--- 1programa/1programa.cpp
+++ 1programa/1programa.cpp
@@ -3,77 +3,80 @@
 #include <string>
 #include <algorithm>
 #include <numeric>
-using std::cout;
-using std::cin;
-using std::endl;
-using std::string;
-using std::setw;
-using std::setprecision;
-using std::left;
-using std::sort;
+#include <vector>
 
 
+using namespace std;
 struct studentas {
     string vardas, pavarde;
-    double nd[10]{};
+    vector <double> nd;
     double egzaminas;
     double galutinis;
 };
 
-void pild(studentas& kint);
-void print(studentas& kin);
+
+void pildyk(vector<studentas>& kint, int& n) //pildymo funkcija
+{
+    cout << "Iveskite studentu skaiciu:" << endl;
+    cin >> n;
+    double sum = 0, laik;
+    double vidurkis = 0;
+    double mediana = 0;
+    for (int i = 0; i < n; i++) {
+        kint.push_back(studentas());
+        cout << "Iveskite studento varda ir pavarde: ";
+        cin >> kint[i].vardas >> kint[i].pavarde;
+        cout << "Iveskite studento namu darbu pazymius: ";
+        while (cin >> laik)
+        {
+            kint[i].nd.push_back(laik);
+            sum += laik;
+        }
+        cin.clear();
+        cin.ignore(10000, '\n');
+        sort(kint[i].nd.begin(), kint[i].nd.end()); //surusiuojame ivestus nd
+        if (n % 2 != 0)
+        {
+            mediana = (double)(kint[i].nd[n / 2]);
+        }
+        else
+        {
+            mediana = (double)((kint[i].nd[(n - 1) / 2]) + (kint[i].nd[n / 2])) / 2.0;
+        }
+        vidurkis = sum / n;
+        cout << "Iveskite egzamino pazymi: "; cin >> kint[i].egzaminas;
+        int m;
+        cout << "Jei norite, jog galutinis pazymys butu skaiciuojamas su vidurkiu iveskite 0, o jei su mediana parasykite 1: "; cin >> m; cout << endl;
+        if (m == 0) {
+            kint[i].galutinis = 0.4 * vidurkis + 0.6 * kint[i].egzaminas;
+        }
+        else
+        {
+            kint[i].galutinis = 0.4 * mediana + 0.6 * kint[i].egzaminas;
+        }
+    }
+
+}
+
+void print(vector<studentas>& kint, int& n) {
+    cout << left << setw(20) << "Pavarde" << setw(20) << "Vardas" << setw(20) << "Galutinis(Vid.) / Galutinis(Med.)" << endl;
+    cout << "--------------------------------------------------------------------------" << endl;
+    for (int i = 0; i < n; i++) {
+        cout << setw(20) << kint[i].pavarde << setw(20) << kint[i].vardas;
+        cout << fixed << setw(20) << setprecision(2) << kint[i].galutinis << endl;
+    }
+
+}
 
 int main()
 {
-    studentas studentai[5];
-    for (int i = 0; i < 2; i++)
-        pild(studentai[i]);
-    for (int i = 0; i < 2; i++)
-        print(studentai[i]);
+    int n;
+    vector <studentas> studentai;
+    pildyk(studentai, n);
+    print(studentai, n);
 }
+ 
 
-void pild(studentas& kint) 
-{
-    int n; 
-    cout << "Iveskite studento varda ir pavarde: "; cin >> kint.vardas >> kint.pavarde;
-    cout << "Kiek namu darbu bus? (1-10): "; cin >> n; 
-    cout << "Iveskite namu darbu pazymius: " << "\n";
-    double sum = 0; 
-    double vidurkis = 0; 
-    double mediana = 0;
-    for (int i = 0; i < n; i++)
-    {
-        cin >> kint.nd[i];
-        sum += kint.nd[i];
-    }
-    sort(kint.nd, kint.nd + n);
-    if (n % 2 != 0)
-    {
-        mediana = (double)kint.nd[n / 2];
-    }
-    else
-    {
-        mediana = (double)(kint.nd[(n - 1) / 2] + kint.nd[n / 2]) / 2.0;
-    }
-    vidurkis = sum / n;
-    cout << "Iveskite egzamino pazymi: "; cin >> kint.egzaminas;
-    int m; 
-    cout << "Jei norite, jog galutinis pazymys butu skaiciuojamas su vidurkiu iveskite 0, o jei su mediana parasykite 1: "; cin >> m; cout << endl;
-    if (m == 0) {
-        kint.galutinis = 0.4 * vidurkis + 0.6 * kint.egzaminas; 
-    }
-    else {
-        kint.galutinis = 0.4 * mediana + 0.6 * kint.egzaminas;
-    }
 
-}
-
-void print(studentas& kint) {
-    cout << left << setw(20) << "Pavarde" << setw(20) << "Vardas" << setw(20) << "Galutinis(Vid.) / Galutinis(Med.)" << endl;
-    cout << "------------------------------------------------------------------" << endl;
-    cout << setw(20) << kint.pavarde << setw(20) << kint.vardas;
-    cout << setw(20) << setprecision(3) << kint.galutinis << endl;
-    cout << "----------------------------------------------------------------------------------------" << endl;
-}
 
 
