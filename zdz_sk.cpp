e2e18455340474974d4f66cc3b3dbd51042462a1
Laineda <90093949+Laineda@users.noreply.github.com>
2021-10-15 00:41:21 +0300

    Delete zdz_sk.cpp

--- zdz_sk.cpp
+++
@@ -1,9 +0,0 @@
-#include "zdz_sk.h"
-#include "studentas.h"
-#include <string>
-#include <sstream>
-
-int zdz_sk(std::string const& str) { //skaiciuojami zodziai
-    std::stringstream stream(str);
-    return std::distance(std::istream_iterator<std::string>(stream), std::istream_iterator<std::string>());
-}
